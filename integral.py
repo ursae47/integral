681625a7e72262f96ca517d39339422fa6a9b537
majorana_neutrino <ursae47@gmail.com>
2021-12-30 10:33:54 -0600

    added comments and improved usability

--- integral.py
+++ integral.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 import math
+import os
+
+# clear screen
+os.system('cls')
 print("***************************************************************")
 print("*                                                             *")
 print("*              Trapezoidal summation program                  *")
@@ -16,13 +20,29 @@ print("*                                  |   |                      *")
 print("*                                  |   v                      *")
 print("*                                                             *")
 print("***************************************************************")
+print("\n")
+print("Enter your definite integral as follows")
+print("1.- Modify the source code to define your function f(x)")
+print("\n")
+print("2.- Enter the upper limit of integration when prompted")
+print("\n")
+print("3.- Enter the lower limit of integration when prompted")
+print("\n")
 
-upper_limit= input('upper boundary value?')
-lower_limit= input('lower boundary value?')
-n=input('number of subintervals?')
- 
+# input values
+upper_limit= input('Upper limit of integration value? (e.g. 3.14159 for pi) ')
+lower_limit= input('Lower limit of integration value? (e.g. 0.00001 for zero) ')
+n=input('number of subintervals? (e.g. 10 or 1000 or 1000000) ')
+
+#casting input strings as floats
+lower_limit=float(lower_limit)
+upper_limit=float(upper_limit)
+n=float(n)
+
+#initializing variable
 integration=0
 
+#simplifying constants & operators
 pi=math.pi
 cos=math.cos
 sin=math.sin
@@ -33,21 +53,22 @@ tan=math.tan
 # I've left pi/2 in the equations so they can be modified in the future
 # to work for more general antennas and not just lambda/2
 
+#evaluating the function at the upper & lower limits
 f_lower_limit=((cos((pi/2)*cos(lower_limit)) - cos(pi/2))**2)/sin(lower_limit)
 f_upper_limit=((cos((pi/2)*cos(upper_limit)) - cos(pi/2))**2)/sin(upper_limit)
 
+#trapezoidal rule for integration
 step_size=(upper_limit-lower_limit)/n
 integration_value=f_lower_limit +f_upper_limit
 
+n=int(n) #casting n to integer so as to use it in the for loop
 for i in range(1,n,1):
 	k=lower_limit + i*step_size
 	f_k=((cos((pi/2)*cos(k)) - cos(pi/2))**2)/sin(k)
 	integration = integration + 2*f_k
-	print(k, f_k, integration,f_upper_limit,f_lower_limit) # used for debugging
+	#print(k, f_k, integration,f_upper_limit,f_lower_limit) # used for debugging
 	i=i+1
 	
 integration = (integration + f_lower_limit + f_upper_limit) *step_size/2
-print(integration)
-
+print("The definite integral's value is: ",integration)
 		
-			
